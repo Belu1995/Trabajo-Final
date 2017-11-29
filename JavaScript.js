0b2ebfe92f864bc9a501b9a08081434bb65bc369
Belu1995 <32782270+Belu1995@users.noreply.github.com>
2017-11-29 15:44:31 -0300

    Cambios en diseño y contenido

--- JavaScript.js
+++ JavaScript.js
@@ -12,20 +12,6 @@ $(document).ready(function(){
 	});
 	
 	
-	$("#pequeñoFabian").click(function(){
-	  $("#sabana2").css("display","block");
-	  $("#ventana2").css("display","block");
-    });
-	
-	$("#pequeñoSebastian").click(function(){
-	  $("#sabana3").css("display","block");
-	  $("#ventana3").css("display","block");
-    });
-	
-	$("#pequeñoSantiago").click(function(){
-	  $("#sabana4").css("display","block");
-	  $("#ventana4").css("display","block");
-    });
 	
 	
 	
